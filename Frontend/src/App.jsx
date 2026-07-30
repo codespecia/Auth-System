08a2d466e85394cc57097d3d886d02dbc9b47e2d
Rakibur Rahman <codespecia@gmail.com>
2026-07-31 03:41:49 +0530

    Frontend Setup Initiated

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Importing Internal Components
+import { LoginCard } from './components';
+
+const App = () => {
+  return (
+    <main>
+      App
+      <LoginCard />
+    </main>
+  );
+};
+
+export default App;
